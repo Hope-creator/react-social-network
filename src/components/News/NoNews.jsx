c70982337cf214ceb5a91161a0cb37c13d1d3c0f
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-01 22:02:03 +0400

    Fix classname

--- src/components/News/NoNews.jsx
+++ src/components/News/NoNews.jsx
@@ -5,7 +5,7 @@ import s from './News.module.css';
 let NoNews = () => {
     return <div className={s.noNewsWrapper}>
         <div className={s.noNewsBody}>
-            <i class="far fa-newspaper"></i>
+            <i className="far fa-newspaper"></i>
             <div>Don't have any news or you don't follow any people</div>
             </div>
     </div>
