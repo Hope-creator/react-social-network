b79f48f1169c3c4861ecc33730d6d14f9e366726
Hope-creator <goryachkin.ivan1@mail.ru>
2021-04-03 14:33:33 +0400

    Some changes for server

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000";
+const URL = "https://boiling-tundra-00901.herokuapp.com/";
 const socket = io(URL, {
     autoConnect: false,
     reconnectionAttempts: 3
