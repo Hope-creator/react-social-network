80924645ef57d660002032052c75aca165b83d34
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-07 11:36:55 +0400

    Create FormContainer

---
+++ src/components/FormContainer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./FormContainer.scss"
+
+const FormContainer = ({children}) => {
+    return (
+        <div className="formContainer">
+            {children}
+        </div>
+    )
+}
+
+export default FormContainer
