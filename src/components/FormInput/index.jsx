f38ef779abc192b319656a5cf1db4ddda9a43ff5
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-07 11:36:37 +0400

    Create FormInput

---
+++ src/components/FormInput/index.jsx
@@ -0,0 +1,18 @@
+import { Field } from "react-final-form";
+
+import "./FormInput.scss";
+
+const FormInput = ({ className, name, validate, placeholder, type }) => (
+  <Field name={name} validate={validate}>
+    {({ input, meta }) => (
+      <div className={className || "formField"}>
+        <input {...input} type={type} placeholder={placeholder} />
+        {meta.error && meta.touched && (
+          <span className="formField__error">{meta.error}</span>
+        )}
+      </div>
+    )}
+  </Field>
+);
+
+export default FormInput;
