d8b22a98bfb50ebd092da6772fe8421216200afa
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-15 20:07:29 +0400

    Make preload as parts component and migrate from css to scss

---
+++ src/parts/preloader/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Preloader.scss";
+
+const Preloader = (props) => {
+  return (
+    <div className="ldsRing">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Preloader;
