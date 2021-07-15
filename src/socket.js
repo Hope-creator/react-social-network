4cf602851c7763413b50ecaa51bbce7350a0dc88
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-15 19:34:01 +0400

    Refactor: socket client

--- src/socket.js
+++ src/socket.js
@@ -1,38 +1,35 @@
 import { io } from "socket.io-client";
+import { dialogsActionCreators } from "./modules/dialogs";
 
 const URL = "http://localhost:5000";
 const socket = io(URL, {
-    autoConnect: false,
-    reconnectionAttempts: 3
+  autoConnect: false,
+  reconnectionAttempts: 3,
 });
 
-const SET_NEW_MESSAGE = 'SET_NEW_MESSAGE';
-const setMessageAction = (newMessage) => ({
-    type: SET_NEW_MESSAGE,
-    newMessage
-})
-
-export const onMessage = (content, to, context) => dispatch => {
-     // if context == 0 message sended not from messages page,
-    // else message send from messages page and message return with conversation
-    socket.emit("message", {
-        content,
-        to,
-        context
-    }, (response)=> {
-        if(response.success && response.message) dispatch((setMessageAction(response.message)))
-    });
-
-}
-
+export const onMessage = (content, to, context) => (dispatch) => {
+  // if context == 0 message sended not from messages page,
+  // else message send from messages page and message return with conversation
+  socket.emit(
+    "message",
+    {
+      content,
+      to,
+      context,
+    },
+    (response) => {
+      if (response.success && response.message)
+        dispatch(dialogsActionCreators.setNewOneMessage(response.message));
+    }
+  );
+};
 
 socket.on("connect_error", (err) => {
-    console.log(err); 
-  });
-  
+  console.log(err);
+});
 
 export const readConversation = (conversationId) => {
-    socket.emit("readConversation", conversationId)
-}
+  socket.emit("readConversation", conversationId);
+};
 
 export default socket;
