6c3f5acd250211cc318d9ab6a360427e27c45a49
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-15 19:29:46 +0400

    Add regUrl to user model

--- server/utils/models/user.js
+++ server/utils/models/user.js
@@ -2,95 +2,99 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: "basic",
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "basic",
+  },
+  status: {
+    type: String,
+    default: "pending",
+  },
+  accepted: {
+    type: Boolean,
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+  },
+  followers: {
+    type: Array,
+    default: [],
+  },
+  regUrl: {
+    type: String,
+    required: false,
+  },
+  profile: {
+    profilePicture: {
+      type: String,
+      default: null,
     },
     status: {
+      type: String,
+      default: null,
+    },
+    aboutMe: {
+      type: String,
+      default: null,
+    },
+    lookingForAJob: {
+      type: Boolean,
+      default: null,
+    },
+    lookingForAJobDescription: {
+      type: String,
+      default: null,
+    },
+    contacts: {
+      github: {
         type: String,
-        default: "pending",
+        default: null,
+      },
+      vk: {
+        type: String,
+        default: null,
+      },
+      facebook: {
+        type: String,
+        default: null,
+      },
+      instagram: {
+        type: String,
+        default: null,
+      },
+      twitter: {
+        type: String,
+        default: null,
+      },
+      website: {
+        type: String,
+        default: null,
+      },
+      youtube: {
+        type: String,
+        default: null,
+      },
+      mainlink: {
+        type: String,
+        default: null,
+      },
     },
-    accepted: {
-        type: Boolean,
-        required: true,
-    },
-    dateCreated: {
-        type: Date,
-        default: Date.now(),
-    },
-    followers: {
-        type: Array,
-        default: []
-    },
-    profile: {
-        profilePicture: {
-            type: String,
-            default: null
-        },
-        status: {
-            type: String,
-            default: null
-        },
-        aboutMe: {
-            type: String,
-            default: null
-        },
-        lookingForAJob: {
-            type: Boolean,
-            default: null
-        },
-        lookingForAJobDescription: {
-            type: String,
-            default: null
-        },
-        contacts: {
-            github: {
-                type: String,
-                default: null
-            },
-            vk: {
-                type: String,
-                default: null
-            },
-            facebook: {
-                type: String,
-                default: null
-            },
-            instagram: {
-                type: String,
-                default: null
-            },
-            twitter: {
-                type: String,
-                default: null
-            },
-            website: {
-                type: String,
-                default: null
-            },
-            youtube: {
-                type: String,
-                default: null
-            },
-            mainlink: {
-                type: String,
-                default: null
-            }
-        }
-    }
+  },
 });
 
 module.exports.User = mongoose.model("user", UserSchema);
