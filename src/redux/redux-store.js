8150690e788503d365202427272e0216cfb5b0d6
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-15 20:31:00 +0400

    Add all modules ducks to store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,27 +1,28 @@
-import {applyMiddleware, combineReducers, createStore, compose} from 'redux';
-import authReducer from '../modules/auth/index';
-import dialogsReducer from './dialogs-reducer';
-import profileReducer from './profile-reducer';
-import usersReducer from './users-reducer';
-import thunkMiddleware from 'redux-thunk';
-import appReducer from './app-reducer';
-import newsReducer from './news-reducer';
-import friendsReducer from './friends-reducer';
+import { applyMiddleware, combineReducers, createStore, compose } from "redux";
+import thunkMiddleware from "redux-thunk";
 
+import authReducer from "../modules/auth";
+import dialogsReducer from "../modules/dialogs";
+import profileReducer from "../modules/profile";
+import usersReducer from "../modules/users";
+import appReducer from "../modules/app";
+import newsReducer from "../modules/news";
+import friendsReducer from "../modules/friends";
 
-let reducers = combineReducers({
-    profilePage: profileReducer,
-    dialogsPage: dialogsReducer,
-    usersPage: usersReducer,
-    auth: authReducer,
-    app: appReducer,
-    newsPage: newsReducer,
-    friendsPage: friendsReducer
+const reducers = combineReducers({
+  profilePage: profileReducer,
+  dialogsPage: dialogsReducer,
+  usersPage: usersReducer,
+  auth: authReducer,
+  app: appReducer,
+  newsPage: newsReducer,
+  friendsPage: friendsReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(
-    applyMiddleware(thunkMiddleware)
-  ));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 export default store;
