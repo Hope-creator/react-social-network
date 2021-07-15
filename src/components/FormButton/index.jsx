1844d287a6d965a483801045cad6dfa070868a32
Hope-creator <goryachkin.ivan1@mail.ru>
2021-07-15 20:02:20 +0400

    Add className prop to FormButton

--- src/components/FormButton/index.jsx
+++ src/components/FormButton/index.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./FormButton.scss";
 
-const FormButton = ({ disabled , type, children}) => {
+const FormButton = ({ disabled, type, children, style }) => {
   return (
     <button
       disabled={disabled}
       className={disabled ? "formButton formButton-disabled" : "formButton"}
       type={type}
+      style={style}
     >
       <span>{children}</span>
     </button>
