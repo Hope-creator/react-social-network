0456f243c9b18b9876cca50d59bd48cf1092a8a5
Hope-creator <goryachkin.ivan1@mail.ru>
2022-02-13 13:51:21 +0400

    Remove necessarily email service usage

--- server/utils/nodemailer.js
+++ server/utils/nodemailer.js
@@ -2,13 +2,20 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const emailService = nodemailer.createTransport({
+const checkEnv = () => {
+  if (!process.env.NODEMAILER_HOST || !process.env.NODEMAILER_PORT || process.env.NODEMAILER_USE || process.env.NODEMAILER_PASS) {
+    return false
+  }
+  return true
+}
+
+const emailService = checkEnv() ? nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
   port: process.env.NODEMAILER_PORT,
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASS,
   },
-});
+}) : null;
 
 module.exports = emailService;
