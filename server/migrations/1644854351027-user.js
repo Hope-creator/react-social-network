c384e13575381f6ad0d261fcb08dc2875580bc70
Hope-creator <goryachkin.ivan1@mail.ru>
2022-02-17 23:31:06 +0400

    Add test user migration

---
+++ server/migrations/1644854351027-user.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const { User } = require("../utils/models/user");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const testEmail = "Test@test.test"
+
+module.exports.up = function (next) {
+
+
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => {
+      console.log("-----> mongoDB connected for migration...");
+      const newUser = new User({
+        name: "Test name",
+        email: testEmail,
+        password: "$2b$10$07dj4AL7Pj9QHawKLkghWe9Om0BsLF2BKTkrLtvzF7rUc59MjuMEO",
+        accepted: true,
+        status: "active"
+      });
+
+      return newUser.save();
+    }).then(() => {
+      console.log("User has been created")
+    })
+    .then(() => next()
+    )
+    .catch((err) =>
+      console.log("-----> Error trying to connect to mongoDB on migration: ", err)
+    );
+
+}
+
+module.exports.down = function (next) {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    })
+    .then(() => {
+      return User.deleteOne({ email: testEmail, });
+    }).then(() => {
+      console.log("User has been deleted")
+    })
+    .then(() => next()
+    )
+    .catch((err) =>
+      console.log("-----> Error trying to connect to mongoDB on migration: ", err)
+    );
+}
